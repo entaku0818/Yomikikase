4de16f7bcba31888a565116f47cbd296974767d7
entaku <entaku19890818@gmail.com>
2024-03-01 02:16:49 +0900

    言語設定のコメント

--- VoiceYourText/setting/LanguageSettingView.swift
+++ VoiceYourText/setting/LanguageSettingView.swift
@@ -13,48 +13,70 @@ struct LanguageSettingView: View {
     let store: Store<SettingsReducer.State, SettingsReducer.Action>
 
     var body: some View {
-        WithViewStore(self.store, observe: { $0 }) {  viewStore in
-            Form {
-                Section(header: Text("Language")) {
-                    Button("Select English") {
-                        viewStore.send(.setLanguage("en"))
-                    }
-                    Button("Select Japanese") {
-                        viewStore.send(.setLanguage("ja"))
+        NavigationView {
+            WithViewStore(self.store, observe: { $0 }) { viewStore in
+                Form {
+                    Section(header: Text("Language")) {
+                        NavigationLink(destination: LanguageSelectionView(store: store)) {
+                            HStack(content: {
+                                Text("Select Language")
+                                Spacer()
+                                Text(viewStore.languageSetting ?? "")
+                            })
+                        }
                     }
                 }
-            }.onAppear(perform: {
-                viewStore.send(.onAppear)
-            })
+                .navigationBarTitle("Settings")
+                .onAppear {
+                    viewStore.send(.onAppear)
+                }
+            }
+        }
+    }
+}
+
+
+struct LanguageSelectionView: View {
+    let store: Store<SettingsReducer.State, SettingsReducer.Action>
+
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            List {
+                ForEach(SettingsReducer.State.availableLanguages, id: \.1) { language in
+                    Button(language.0) {
+                        viewStore.send(.setLanguage(language.1))
+                    }
+                }
+            }
+            .navigationBarTitle("Select Language", displayMode: .inline)
         }
     }
 }
 
 
 struct SettingsReducer: Reducer {
-
-    
     struct State: Equatable {
-        var languageSetting: String? // 言語設定を保持するためのプロパティ
+        var languageSetting: String?
+        static let availableLanguages = [("English", "en"), ("Japanese", "ja")]
     }
 
     enum Action: Equatable, Sendable {
-        case setLanguage(String?) // 言語設定を変更するアクション
-        case onAppear // UserDefaultsから言語設定を読み込むアクション
+        case setLanguage(String?)
+        case onAppear
     }
-
-
 
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .setLanguage(let languageCode):
-                state.languageSetting = languageCode
-                // UserDefaultsに言語設定を保存
                 UserDefaultsManager.shared.languageSetting = languageCode
+                if let code = languageCode, let languageName = SettingsReducer.State.availableLanguages.first(where: { $0.1 == code })?.0 {
+                    state.languageSetting = languageName
+                } else {
+                    state.languageSetting = "English"
+                }
                 return .none
             case .onAppear:
-                // UserDefaultsから言語設定を読み込み、stateを更新
                 state.languageSetting = UserDefaultsManager.shared.languageSetting
                 return .none
             }
