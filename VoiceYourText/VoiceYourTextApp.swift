b75a250e5b06ceb125ebe5612deb597e7bad06bf
entaku <entaku19890818@gmail.com>
2024-03-10 16:15:08 +0900

    add google info

--- VoiceYourText/VoiceYourTextApp.swift
+++ VoiceYourText/VoiceYourTextApp.swift
@@ -8,11 +8,12 @@
 import ComposableArchitecture
 import SwiftUI
 import UIKit
+import FirebaseCore
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        FirebaseApp.configure()
         let languageCode:String = UserDefaultsManager.shared.languageSetting ?? "en"
         UserDefaults.standard.set([languageCode], forKey: "AppleLocale")
         UserDefaults.standard.synchronize()
